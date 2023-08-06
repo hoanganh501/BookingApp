9675641b5739a0bb1b191ab35fbf59e6749107d8
hoanganh501 <lhoanga99@gmail.com>
2023-08-06 22:30:49 +0700

    Login and register page

---
+++ api/index.js
@@ -0,0 +1,85 @@
+import express from 'express';
+import cors from "cors";
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+
+import User from './models/User.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const port = 8000
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtToken = 'quxisjaopnbasw';
+
+app.use(cors({
+  credentials: true,
+  origin: 'http://127.0.0.1:5173',
+}));
+
+mongoose.connect(process.env.MONGO_URL);
+
+app.get('/test', (req, res) => {
+    res.json('test ok');
+  })
+
+  app.post('/register',async (req,res) => {
+    const {name,email,password} = req.body;
+    try {
+      const userDoc = await User.create({
+        name,
+        email,
+        password:bcrypt.hashSync(password, bcryptSalt),
+      });
+    res.json(userDoc);
+    } catch (error) {
+      res.status(422).json(error);
+    }
+  });
+  
+  app.post('/login',async (req,res) => {
+    const {email,password} = req.body;
+      const userDoc = await User.findOne({email});
+      if (userDoc) {
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if (passOk) {
+          jwt.sign({
+            email: userDoc.email,
+            id: userDoc._id,
+          }, jwtToken, {}, (error,token) => {
+            if (error) throw error;
+            res.cookie('token',token).json(userDoc);
+          }); 
+        } else {
+          res.status(422).json('pass not ok');
+        }
+      } else {
+        res.json('not found');
+      }
+  });
+
+
+  app.get('/profile', (req,res) => {
+    const {token} = req.cookies;
+    if (token) {
+      jwt.verify(token, jwtToken, {}, async (error, userData) => {
+        if (error) throw error;
+        const {name,email,_id} = await User.findById(userData.id)
+        res.json({name,email,_id});
+      });
+    } else {
+      res.json(null);
+    }
+
+  })
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port: http://localhost:${port}`)
+  })
---
+++ client/src/templates/pages/IndexPage.tsx
@@ -0,0 +1,8 @@
+import Header from "../layouts/Header";
+
+
+export default function IndexPage() {
+    return (
+        <div>indexpage</div>
+    );
+}
